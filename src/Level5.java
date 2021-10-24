55f5821e7b8618f27b3ac057484fc8fd541abe03
laowang <2915494647@qq.com>
2021-10-24 09:40:18 +0800

    fist work modification

--- src/Level5.java
+++ src/Level5.java
@@ -3,51 +3,11 @@ import java.util.Scanner;
 public class Level5 {
     public static void lv5() {
         MutableArraylist list = new MutableArraylist();
-        int flag = 1;
-        do {
-            System.out.println("请选择使用的功能");
-            System.out.println("1.添加一个元素给动态容器中");
-            System.out.println("2.得到某个索引下的元素");
-            System.out.println("3.删除某个索引下的元素");
-            System.out.println("4.展示动态数组的元素");
-            System.out.println("5.删除指定元素");
-            System.out.println("6.在对应位置添加下标");
-            System.out.println("7.退出程序");
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            switch (n) {
-                case 1:
-                    System.out.println("请输入要加入的元素");
-                    String x = in.next();
-                    list.add(x);
-                    break;
-                case 2:
-                    System.out.println("请输入元素下标");
-                    int c = in.nextInt();
-                    System.out.println(list.get(c));
-                    break;
-                case 3:
-                    System.out.println("请输入要删除元素的下标");
-                    int d = in.nextInt();
-                    list.delete(d);
-                    break;
-                case 4:
-                    list.show();
-                    break;
-                case 5:
-                    System.out.println("请输入要删除的元素");
-                    Object r = in.next();
-                    list.remove(r);
-                case 6:
-                    System.out.println("请输入要加入元素的下标");
-                    int a = in.nextInt();
-                    list.add(a);
-                    break;
-                case 7:
-                    flag = 0;
-                    break;
-            }
-        } while (flag == 1);
+        list.add("红岩网校冲冲冲");
+        list.delete(0);
+        list.add(0, "加油冲冲冲");
+        System.out.println(list.get(0));
+        System.out.println("list有" + list.size() + "个元素");
     }
 }
 
@@ -61,12 +21,13 @@ class MutableArraylist {
 
     //默认容量为1
     public MutableArraylist() {
+
         this(1);
     }
 
     //添加一个元素到数组中
     public void add(String add) {
-        if (size== elementDate.length) {
+        if (size == elementDate.length) {
             this.resize(elementDate.length + 1);
         }
         elementDate[size] = add;
@@ -86,7 +47,10 @@ class MutableArraylist {
     }
 
     public void delete(int index) {
-        for (int i = size; i > index; i--) {
+        for (int i = size - 1; i >= index; i--) {
+            if (i == 0){
+                break;
+            }
             elementDate[i - 1] = elementDate[i];
         }
         size--;
@@ -98,7 +62,7 @@ class MutableArraylist {
         }
     }
 
-    public void remove(Object index) {
+    public void delete(Object index) {
         for (int i = 0; i < size; i++) {
             if (index == elementDate[i]) {
                 delete(i);
@@ -109,7 +73,7 @@ class MutableArraylist {
         }
     }
 
-    public void add(int index) {
+    public void add(int index, Object a) {
         if (size == elementDate.length) {
             this.resize(elementDate.length + 1);
         }
@@ -119,9 +83,11 @@ class MutableArraylist {
         for (int i = 0; i < elementDate.length; i++) {
             System.out.println(elementDate[i]);
         }
-        System.out.println("请输入要加入的元素");
-        String a = new Scanner(System.in).next();
         elementDate[index] = a;
         size++;
+    }
+
+    public int size() {
+        return size;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Level1.lv1();
-        Level2.lv2();
-        Level3.lv3();
-        Level4.lv4();
-        Level5.lv5();
+        show();
+        Scanner in = new Scanner(System.in);
+        int flag = in.nextInt();
+        do {
+            switch (flag) {
+                case 1:
+                    Level1.lv1();
+                    break;
+                case 2:
+                    Level2.lv2();
+                    break;
+                case 3:
+                    Level3.lv3();
+                    break;
+                case 4:
+                    Level4.lv4();
+                    break;
+                case 5:
+                    Level5.lv5();
+                    break;
+            }
+            show();
+            flag = in.nextInt();
+            show();
+        } while (flag != 0);
+    }
+
+
+    public static void show() {
+        System.out.println("输入数字");
+        System.out.println("1:lv1");
+        System.out.println("2:lv2");
+        System.out.println("3:lv3");
+        System.out.println("4:lv4");
+        System.out.println("5:lv5");
+        System.out.println("0:exit");
     }
 }
